cf029e5275776cc4485f8ed918b4db808d868720
john-smilga <johnsmilga@gmail.com>
2020-03-16 10:09:48 -0700

    1-store-created

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+
+// store - stores data, think of state
+// reducer - function that used to update store
+
+import { createStore } from "redux";
+// reducer
+function reducer() {
+  console.log("shake and bake");
+}
+const store = createStore(reducer);
+
+function App() {
+  // cart setup
+
+  return (
+    <main>
+      <Navbar />
+      <CartContainer cart={cartItems} />
+    </main>
+  );
+}
+
+export default App;
