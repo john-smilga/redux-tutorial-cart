baa5afa9559d45cc9e6e15cb0ea76a7fc4411d2d
john-smilga <johnsmilga@gmail.com>
2020-03-17 11:08:31 -0700

    18-dispatch-clear-cart

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,7 +1,8 @@
 import React from "react";
 import CartItem from "./CartItem";
 import { connect } from "react-redux";
-const CartContainer = ({ cart = [], total }) => {
+import { CLEAR_CART } from "../actions";
+const CartContainer = ({ cart = [], total, dispatch }) => {
   if (cart.length === 0) {
     return (
       <section className="cart">
@@ -33,7 +34,12 @@ const CartContainer = ({ cart = [], total }) => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button
+          className="btn clear-btn"
+          onClick={() => dispatch({ type: CLEAR_CART })}
+        >
+          clear cart
+        </button>
       </footer>
     </section>
   );
--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,9 @@
-import { DECREASE, INCREASE } from "./actions";
+import { DECREASE, INCREASE, CLEAR_CART } from "./actions";
 
 function reducer(state, action) {
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
+  }
   return state;
 }
 
