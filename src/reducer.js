bcbfe9874bc962b816bacc4ab68e353d7cec9211
john-smilga <johnsmilga@gmail.com>
2020-03-17 11:21:09 -0700

    20-reducer-setup

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,19 @@
-import { DECREASE, INCREASE, CLEAR_CART } from "./actions";
+import { DECREASE, INCREASE, CLEAR_CART, REMOVE } from "./actions";
 
 function reducer(state, action) {
   if (action.type === CLEAR_CART) {
     return { ...state, cart: [] };
   }
+  if (action.type === DECREASE) {
+    console.log("you decreased amount");
+  }
+  if (action.type === INCREASE) {
+    console.log("you increased amount");
+  }
+  if (action.type === REMOVE) {
+    console.log("you remove amount");
+  }
+
   return state;
 }
 
