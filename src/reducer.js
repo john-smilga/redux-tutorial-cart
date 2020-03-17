32710079f862f804570de530fbacd183c1a8f74c
john-smilga <johnsmilga@gmail.com>
2020-03-17 12:42:23 -0700

    25-decrease-functionality

--- src/reducer.js
+++ src/reducer.js
@@ -5,7 +5,20 @@ function reducer(state, action) {
     return { ...state, cart: [] };
   }
   if (action.type === DECREASE) {
-    console.log("you decreased amount");
+    let tempCart = [];
+    if (action.payload.amount === 1) {
+      tempCart = state.cart.filter(
+        cartItem => cartItem.id !== action.payload.id
+      );
+    } else {
+      tempCart = state.cart.map(cartItem => {
+        if (cartItem.id === action.payload.id) {
+          cartItem = { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      });
+    }
+    return { ...state, cart: tempCart };
   }
   if (action.type === INCREASE) {
     let tempCart = state.cart.map(cartItem => {
@@ -19,7 +32,7 @@ function reducer(state, action) {
   if (action.type === REMOVE) {
     return {
       ...state,
-      cart: state.cart.filter(cartItem => cartItem.id !== action)
+      cart: state.cart.filter(cartItem => cartItem.id !== action.payload.id)
     };
   }
 
