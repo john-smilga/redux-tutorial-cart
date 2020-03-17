264236a27b0e55cba102196a5b79836724389d9e
john-smilga <johnsmilga@gmail.com>
2020-03-17 11:14:21 -0700

    19-switch-statement

--- src/reducer.js
+++ src/reducer.js
@@ -8,3 +8,10 @@ function reducer(state, action) {
 }
 
 export default reducer;
+
+// switch (action.type) {
+//   case CLEAR_CART:
+//     return { ...state, cart: [] };
+//   default:
+//     return state;
+// }
